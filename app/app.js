6a56f694ba5be2e45cbe9d8d5e7f96e0eeda1692
Vlad <vvllaadd1233345@gmail.co>
2024-01-28 14:13:34 +0100

    deleted not needed routes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
-const wrapper = require("./helpers/wrapper");
-const express = require("express");
-const error = require("./helpers/error");
+const wrapper = require('./helpers/wrapper');
+const express = require('express');
+const error = require('./helpers/error');
 const logger = require('morgan');
 const cors = require('cors');
 
@@ -10,24 +10,8 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.get(
-  "/products",
-  wrapper((req, res) => {
-    const happenedError = error(401, "Error_1");
-    throw happenedError;
-  })
-);
-
-app.get(
-  "/users",
-  wrapper((req, res) => {
-    const happenedError = error(501, "Error_2");
-    throw happenedError;
-  })
-);
-
 app.use((_, res) => {
-  res.status(404).json({ message: "not found" });
+    res.status(404).json({ message: 'not found' });
 });
 
 module.exports = app;
